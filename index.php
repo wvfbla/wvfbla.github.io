8eed297013264db0c6f976510f97e3e47dfd6cfe
wvfbla <komalanerurkar@gmail.com>
2015-09-07 01:48:10 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,16 +8,13 @@
 
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
     
-    
-    
-?>
-<?php
     if ($_POST['submit']) {
-    if (mail ($to, $subject, $body, $from)) { 
-        echo '<p>Your message has been sent!</p>';
-    } else { 
-        echo '<p>Something went wrong, go back and try again!</p>'; 
+        if (mail ($to, $subject, $body, $from)) { 
+            echo '<p>Your message has been sent!</p>';
+        }
+        else { 
+            echo '<p>Something went wrong, go back and try again!</p>'; 
+        }
     }
-}
 
 ?>
