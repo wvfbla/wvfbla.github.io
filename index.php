65dfc2c0ef04f9bf2b5f71efb6e5de26feb7af8e
wvfbla <komalanerurkar@gmail.com>
2015-09-07 02:01:26 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,8 @@
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
     
     if ($_POST['submit']) {
-        if (mail ($to, $subject, $body, $from)) { 
-            echo '<p>Your message has been sent!</p>';
-        }
-        else { 
-            echo '<p>Something went wrong, go back and try again!</p>'; 
-        }
+        mail ($to, $subject, $body, $from);
+        
     }
 
 ?>
